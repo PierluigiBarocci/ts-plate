01b6843ee86bc888dcfdcaf2eeef12374d232895
Pierluigi Barocci <pierluigi.barocci@nautes.com>
2022-03-18 09:58:12 +0100

    aut deleting unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  const a = 'Testing Unused import and var';
   return (
     <div className={styles.container}>
       <Head>
